6ef134d81b612bf597a21ea3ce0b4638af583b69
Gregory Jacobs <greg@greg-jacobs.com>
2013-04-13 16:22:38 -0400

    Have Converter accept the filepath to report parse errors with.

--- src/Converter.js
+++ src/Converter.js
@@ -10,16 +10,18 @@ var Converter = Class.extend( Object, {
 	 * Performs the full conversion, applying all transformations.
 	 * 
 	 * @param {String} input The input string to convert. Usually the contents of the file to convert.
+	 * @param {String} filePath The path of the file being processed. This is just used for debugging, to show the 
+	 *   filename when a parse error occurs.
 	 * @return {String} The converted output.
 	 */
-	convert : function( input ) {
+	convert : function( input, filePath ) {
 		var str = input;
 		
 		// First, convert the JSHint Globals
 		str = this.convertJsHintGlobals( str );
 		
 		// Now parse the tests, and apply transformations
-		var parseResult = this.parse( str ),
+		var parseResult = this.parse( str, filePath ),
 		    parseTree = parseResult.getParseTree();
 		
 		var jasmineWriter = new JasmineWriter( {
@@ -82,10 +84,12 @@ var Converter = Class.extend( Object, {
 	 * Parses the `input`, and returns a {@link ParseResult} which has the tree of parse {@link node.Node Nodes}.
 	 * 
 	 * @param {String} input
+	 * @param {String} filePath The path of the file being processed. This is just used for debugging, to show the 
+	 *   filename when a parse error occurs.
 	 * @return {ParseResult}
 	 */
-	parse : function( input ) {
-		return new Parser( input ).parse();
+	parse : function( input, filePath ) {
+		return new Parser( input, filePath ).parse();
 	}
 	
 } );
